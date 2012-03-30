a9967c301c17c41a1db195221686871e6265c0f2
Mike Hugo <mike@entagen.com>
2012-03-30 12:32:00 -0500

    sample JS

--- BuildingQuality/sample-code/metrics/web-app/js/application.js
+++ BuildingQuality/sample-code/metrics/web-app/js/application.js
@@ -7,3 +7,27 @@ if (typeof jQuery !== 'undefined') {
 		});
 	})(jQuery);
 }
+
+function initPeriodicalUpdater(url, completionUrl) {
+    var periodicalUpdaterHandler = $.PeriodicalUpdater(url, {
+        method: 'get',          // method; get or post
+        data: '',                   // array of values to be passed to the page - e.g. {name: "John", greeting: "hello"}
+        minTimeout: 1000,       // starting value for the timeout in milliseconds
+        maxTimeout: 8000,       // maximum length of time between requests
+        multiplier: 2,          // if set to 2, timerInterval will double each time the response hasn't changed (up to maxTimeout)
+        type: 'json',           // response type - text, xml, json, etc.  See $.ajax config options
+        maxCalls: 0,            // maximum number of calls. 0 = no limit.
+        autoStop: 0             // automatically stop requests after this many returns of the same data. 0 = disabled.
+    }, function(data) {
+        $('.importPercentComplete').html(data.text);
+        $('#progressBar').progressbar("value", parseInt(data.value));
+        if (100 == parseInt(data.value)) {
+            periodicalUpdaterHandler.stop();
+            $('#progressBar').siblings('.close').click();
+            if (completionUrl) {
+                window.location = completionUrl;
+            }
+        }
+    });
+
+}
